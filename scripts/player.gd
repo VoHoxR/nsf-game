da2df960d59668482a389d3dc1065288a1c5799c
VoHoxR <148103619+VoHoxR@users.noreply.github.com>
2024-11-11 11:00:45 -0600

    Added basic 3d movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+@onready var head: Node3D = $head
+
 
 var current_speed = 5.0
 
@@ -9,15 +11,36 @@ const crouching_speed = 3.0
 
 const jump_vel = 4.5
 
-const mouse_sens = 0.4
+const mouse_sens = 0.2
 
+var lerp_speed = 10.0
 
+var direction = Vector3.ZERO
+
+var crouching_depth = -0.8
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-deg_to_rad(event.relative.x * mouse_sens))
+		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
+		head.rotation.x = clamp(head.rotation.x,deg_to_rad(-50),deg_to_rad(60))
+		
+		
 func _physics_process(delta: float) -> void:
-	
-	if Input.is_action_pressed("sprint"):
-		current_speed = sprinting_speed
+	if Input.is_action_pressed("crouch"):
+		current_speed = crouching_speed
+		head.position.y = lerp(head.position.y,1.8 + crouching_depth,delta*lerp_speed)
 	else:
-		current_speed = walking_speed
+		head.position.y = lerp(head.position.y,1.8,delta*lerp_speed)
+		if Input.is_action_pressed("sprint"):
+			current_speed = sprinting_speed
+		else:
+			current_speed = walking_speed
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -29,8 +52,9 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(),delta*lerp_speed)
+	
 	if direction:
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
